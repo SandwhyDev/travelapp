01c70078245bdf9abbbad83fb97b06baddcb0f2a
SandwhyDev <sandwhydev@gmail.com>
2022-06-03 15:54:16 +0700

    up

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
-import React,{useEffect,useState} from 'react'
-import Wellcome from '../components/Wellcome';
-import SplashScreen from './splashscreen';
+import { Head } from "next/document";
+import React, { useEffect, useState } from "react";
+import Wellcome from "../components/Wellcome";
+import SplashScreen from "./splashscreen";
 const Home = () => {
   const [Loading, setLoading] = useState(false);
 
@@ -11,7 +12,12 @@ const Home = () => {
     }, 3000);
   }, []);
   return (
-    <div className='w-screen h-screen flex flex-col justify-center items-center bg-[#F3F5F6]'>
+    <div className="w-screen h-screen flex flex-col justify-center items-center bg-[#F3F5F6]">
+      <Head>
+        <meta property="og:image" content="traveler.png" />
+        <meta name="description" content="Aplikasi travel" />
+        <link rel="shortcut icon" href="traveler.png" type="image/png" />
+      </Head>
       {Loading ? (
         <Wellcome />
       ) : (
@@ -20,7 +26,7 @@ const Home = () => {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
